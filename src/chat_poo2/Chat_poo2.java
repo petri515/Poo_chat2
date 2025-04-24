9c214c229e76b0bca56a1117d21a716305d91f8f
estudiante <estudiante@10.220.4.121>
2025-04-24 16:52:28 -0500

    1.saludos

---
+++ src/chat_poo2/Chat_poo2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chat_poo2;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Chat_poo2 {
+
+    /**
+     * 1.Hola
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
